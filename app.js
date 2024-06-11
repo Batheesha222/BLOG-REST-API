ce5e83de3f316e8c30bbac02fd37ef4ae2389247
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-11 23:02:07 +0530

    connect to database

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const dotenv = require("dotenv")
-
+const dotenv = require("dotenv");
 dotenv.config();
+const connectMongodb = require("./init/mongodb")
 
 //init app
 const app = express();
 
+//connect database
+connectMongodb();
+
 //third party middleware
-app.use(express.json({limit:"500mb"}));
-app.use(bodyParser.urlencoded({limit:"500mb",extended:true}));
+app.use(express.json({limit:"500mb"}));//limit 500mb to passing DATA
+app.use(bodyParser.urlencoded({limit:"500mb",extended:true}));//extended:true because provide a warning
 
 module.exports = app;
