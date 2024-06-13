f08e23681808be76461f42c8fe3f5221749bfe49
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-13 19:56:39 +0530

    install bcrypt, hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const {User} = require("../models")
-
+const hashPassword = require("../utils/hashPassword")
 const signup = async (req,res,next) =>{
     try {
         const {name,email,password,role} = req.body;
@@ -9,7 +9,10 @@ const signup = async (req,res,next) =>{
             res.code = 400;
             throw new Error("Email already exist")
         }
-        const newUser = new User({name,email,password,role})
+
+        const hashedPassword = await hashPassword(password)
+    
+        const newUser = new User({name,email,password:hashedPassword,role})
 
         await newUser.save();
         res.status(201).json({code :201,status:true, message:"User registered successfully"})
