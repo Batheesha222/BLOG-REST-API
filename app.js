fc5a84d51e41cbbd76bccb05c67d20c74e503256
Batheesha222 <batheesha.dilmeen@gmail.com>
2024-06-18 19:52:52 +0530

    Add post, Update post, delete post  , post list and filter a post

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const connectMongodb = require("./init/mongodb");
-const { authRoute, categoryRoute, fileRoute } = require("./routes");
+const { authRoute, categoryRoute, fileRoute, postRoute } = require("./routes");
 const morgan = require("morgan");
-const {errorHandler} = require("./middleware");
-const notfound = require("./controllers/notFound")
+const { errorHandler } = require("./middleware");
+const notfound = require("./controllers/notFound");
 
 //init app
 const app = express();
@@ -21,15 +21,14 @@ app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/category", categoryRoute)
-app.use("/api/v1/file", fileRoute)
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/file", fileRoute);
+app.use("/api/v1/posts", postRoute);
 
 //not found route
-app.use("*",notfound)
+app.use("*", notfound);
 
 //errorHandlers middleware
 app.use(errorHandler);
-
-
 
 module.exports = app;
